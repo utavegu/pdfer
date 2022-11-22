342fb26498f9391f1226831f4590693b28e6483e
Utavegu <utavegu@list.ru>
2022-11-22 21:00:53 +0500

    chore: edit readme

--- src/index.js
+++ src/index.js
@@ -9,13 +9,3 @@ console.log(process.env)
 console.log(`http://localhost:${PORT}`)
 
 app.listen(PORT);
-
-// docker rm pdfer-container (если останавливать через docker-compose down, то не понадобится)
-// docker rmi pdfer-wtf:latest
-// docker-compose up
-// --build
-
-// docker exec -it pdfer-container /bin/sh
-// /bin/bash не сработает, так как alpine, видимо
-
-// Добавь еще сюда команду как дев и прод композы запускать и собственно внедри это сюда. Прод - просто дефолтное название.
